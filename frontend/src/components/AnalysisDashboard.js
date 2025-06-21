3b71ea88e05602ac9afa339eaa73e32fb4a19ca4
erato11131113 <142071872+yitingzhang1113@users.noreply.github.com>
2025-06-21 16:58:42 -0700

    initinal

---
+++ frontend/src/components/AnalysisDashboard.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AnalysisDashboard = () => <div><h2>Analysis Dashboard</h2><p>This section will display emotional data analysis reports over a period of time to help parents and teachers better understand the child's situation.</p></div>;
+
+export default AnalysisDashboard; 
